a785b760d51bb45ca6f2e92072f5422bd8b877cc
angeline <mirandaangelinetricia@gmail.com>
2026-02-03 10:24:00 +0800

    added layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <v-btn>Submit</v-btn>
+    <v-row>
+     <v-col cols="3" md="3">
+      <v-card height="60" color="primary"> </v-card>
+     </v-col>
+     
+     <v-col cols="3" md="3">
+      <v-card height="60" color="blue"> </v-card>
+     </v-col>
+    
+     <v-col cols="3" md="3">
+      <v-card height="60" color="green"> </v-card>
+     </v-col>
+     
+     <v-col cols="3" md="3">
+      <v-card height="60" color="purple"> </v-card>
+     </v-col>
+    </v-row>
+
   </div>
 </template>
 
